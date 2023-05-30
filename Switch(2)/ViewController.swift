e78c78f3ec45a5d38e89affdc4edad8983156150
ChauveSouris <desseobscurite@gmail.com>
2023-05-30 22:09:32 +0300

    Created App

--- Switch(2)/ViewController.swift
+++ Switch(2)/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setView()
+        setSlider()
+      
+        colorView.layer.cornerRadius = 15
     }
 
-
+    @IBAction func actionSlider(_ sender: UISlider) {
+        setView()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: sender)
+        case greenSlider:
+            greenLabel.text = string(from: sender)
+        default:
+            blueLabel.text = string(from: sender)
+        }
+    }
+    
+    
+    private func setSlider() {
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
+    }
+    
+    private func setView() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 0.5
+        )
+    }
+    
+    private func string(from slider: UISlider ) -> String {
+        String(format: "%.1f", slider.value)
+    }
+    
 }
 
