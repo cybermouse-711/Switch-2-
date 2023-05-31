88c1705e24892baec102e08e71abafd51c750acc
ChauveSouris <desseobscurite@gmail.com>
2023-06-01 00:30:22 +0300

    Added Protocol

--- Switch(2)/ViewConrolers/ColorViewController.swift
+++ Switch(2)/ViewConrolers/ColorViewController.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 
+protocol SettingViewContrrollerDelegate: AnyObject {
+    func setColor(_ color: RGBColor)
+}
+
 final class ColorViewController: UIViewController {
 
+    private var color = RGBColor(
+        redColor: 0,
+        greenColor: 0,
+        blueColor: 0,
+        alfa: 0.5
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingVC = segue.destination as? SettingViewController else { return }
+        settingVC.color = color
+        settingVC.delegate = self
+    }
 
     @IBAction func settingButton(_ sender: UIBarButtonItem) {
+    }
+}
+
+// MARK: - SettingViewContrrollerDelegate
+extension ColorViewController: SettingViewContrrollerDelegate {
+    func setColor(_ color: RGBColor) {
+       self.color = color
     }
 }
