4445aeab9151ff262024abcba92e99855724b0fc
ChauveSouris <desseobscurite@gmail.com>
2023-06-01 00:29:40 +0300

    Incorected Model

--- Switch(2)/Models/RGBColor.swift
+++ Switch(2)/Models/RGBColor.swift
@@ -12,25 +12,5 @@ struct RGBColor {
     var greenColor: Double
     var blueColor: Double
     var alfa: Double
-    
-  /*
-    static func getColorInt() {
-        
-        switch _ {
-        case .red:
-            String(format: "%.1f", redColor)
-        case .green:
-            String(format: "%.1f", greenColor)
-        case .blue:
-            String(format: "%.1f", blueColor)
-        }
-    }
-    */
-    
-    enum Color {
-        case red
-        case green
-        case blue
-    }
-    
+   
 }
