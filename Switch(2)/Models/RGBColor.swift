0c8985bca252708ce17ffb6be221544775227cc2
ChauveSouris <desseobscurite@gmail.com>
2023-06-02 01:11:00 +0300

    Incorected Struct

--- Switch(2)/Models/RGBColor.swift
+++ Switch(2)/Models/RGBColor.swift
@@ -5,12 +5,26 @@
 //  Created by Елизавета Медведева on 30.05.2023.
 //
 
+import UIKit
+
 
 struct RGBColor {
     
-    var redColor: Double
-    var greenColor: Double
-    var blueColor: Double
-    var alfa: Double
-   
+    var redColor: Float 
+    var greenColor: Float
+    var blueColor: Float
+    var alfa: Float
+    
+    func string(_ color: UISlider ) -> String {
+        String(format: "%.1f", color.value)
+    }
+    
+    func setColorRGB(redColor: Float, greenColor: Float, blueColor: Float, alfa: Float) -> UIColor {
+        UIColor(
+            red: CGFloat(redColor),
+            green: CGFloat(greenColor),
+            blue: CGFloat(blueColor),
+            alpha: CGFloat(alfa)
+        )
+    }
 }
